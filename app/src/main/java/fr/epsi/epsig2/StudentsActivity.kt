1b6cab9a1306e3858636e16fa1e0d16c4a33030a
DJEMAM Nidal <ndjemam@snapp.fr>
2021-12-07 16:05:23 +0100

    update info

--- app/src/main/java/fr/epsi/epsig2/StudentsActivity.kt
+++ app/src/main/java/fr/epsi/epsig2/StudentsActivity.kt
@@ -14,26 +14,41 @@ class StudentsActivity : BaseActivity() {
             "    {\n" +
             "      \"picture_url\": \"https://www.numerama.com/content/uploads/2019/05/trou-noir-espace-univers-astronomie.jpg\",\n" +
             "      \"name\": \"Allan\",\n" +
+            "      \"city\": \"bordeaux\",\n" +
+            "      \"zipcode\": \"33000\",\n" +
+            "      \"phone\": \"0619191919\",\n" +
             "      \"email\": \"allan@epsi.fr\"\n" +
             "    },\n" +
             "    {\n" +
             "      \"picture_url\": \"https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg\",\n" +
             "      \"name\": \"Arraud\",\n" +
+            "      \"city\": \"bordeaux\",\n" +
+            "      \"zipcode\": \"33000\",\n" +
+            "      \"phone\": \"0619191919\",\n" +
             "      \"email\": \"arraud@epsi.fr\"\n" +
             "    },\n" +
             "    {\n" +
             "      \"picture_url\": \"https://media.gettyimages.com/photos/colorful-powder-explosion-in-all-directions-in-a-nice-composition-picture-id890147976?s=2048x2048\",\n" +
             "      \"name\": \"Nicolas\",\n" +
+            "      \"city\": \"bordeaux\",\n" +
+            "      \"zipcode\": \"33000\",\n" +
+            "      \"phone\": \"0619191919\",\n" +
             "      \"email\": \"nicolas@epsi.fr\"\n" +
             "    },\n" +
             "    {\n" +
             "      \"picture_url\": \"https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg\",\n" +
             "      \"name\": \"Lilian\",\n" +
+            "      \"city\": \"bordeaux\",\n" +
+            "      \"zipcode\": \"33000\",\n" +
+            "      \"phone\": \"0619191919\",\n" +
             "      \"email\": \"lilian@epsi.fr\"\n" +
             "    },\n" +
             "    {\n" +
             "      \"picture_url\": \"https://cdn.futura-sciences.com/buildsv6/images/largeoriginal/2/3/1/2310c9171a_50157784_pia23441.jpg\",\n" +
             "      \"name\": \"Maxime\",\n" +
+            "      \"city\": \"bordeaux\",\n" +
+            "      \"zipcode\": \"33000\",\n" +
+            "      \"phone\": \"0619191919\",\n" +
             "      \"email\": \"maxime@epsi.fr\"\n" +
             "    }\n" +
             "  ]\n" +
